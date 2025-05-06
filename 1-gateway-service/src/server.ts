6840088895e8764c1427397778d99eaa0d54d8da
IacobAlexandruGeorgian <iacobalex11@yahoo.com>
2025-05-06 05:22:34 +0300

    test

--- 1-gateway-service/src/server.ts
+++ 1-gateway-service/src/server.ts
@@ -1,6 +1,6 @@
 import http from 'http';
 
-// import 'express-async-errors';
+import 'express-async-errors';
 import { CustomError, IErrorResponse, winstonLogger } from '@iacobalexandrugeorgian/shared';
 import { Application, Request, Response, json, urlencoded, NextFunction } from 'express';
 import { Logger } from 'winston';
@@ -21,6 +21,9 @@ import { Server } from 'socket.io';
 import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { SocketIOAppHandler } from '@gateway/sockets/socket';
+import { axiosMessageInstance } from '@gateway/services/api/message.service';
+import { axiosOrderInstance } from '@gateway/services/api/order.service';
+import { axiosReviewInstance } from '@gateway/services/api/review.service';
 import { isAxiosError } from 'axios';
 
 const SERVER_PORT = 4000;
@@ -71,6 +74,9 @@ export class GatewayServer {
         axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosReviewInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
       }
       next();
     });
